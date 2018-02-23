75b07dfc18377f1577bccb76370e37f8874e7ee2
tysonparks <typarks@gmail.com>
2018-02-23 14:44:55 -0800

    Created Calculator class

---
+++ TipCalculator/Calculator.h
@@ -0,0 +1,13 @@
+//
+//  Calculator.h
+//  TipCalculator
+//
+//  Created by Tyson Parks on 2/23/18.
+//  Copyright © 2018 Tyson Parks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Calculator : NSObject
+
+@end
---
+++ TipCalculator/Calculator.m
@@ -0,0 +1,13 @@
+//
+//  Calculator.m
+//  TipCalculator
+//
+//  Created by Tyson Parks on 2/23/18.
+//  Copyright © 2018 Tyson Parks. All rights reserved.
+//
+
+#import "Calculator.h"
+
+@implementation Calculator
+
+@end
