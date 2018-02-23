adf32538121fcd46bd6e260cba39eb774b1495e2
tysonparks <typarks@gmail.com>
2018-02-23 14:44:16 -0800

    Commented code with planning to create model in new class

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -10,18 +10,20 @@
 
 @interface ViewController () /* <UITextFieldDelegate> */
 
+// Labeling properties
 @property (strong, nonatomic) NSString *tipPercentage;
 @property (strong, nonatomic) NSString *tipAmount;
 @property (strong, nonatomic) NSString *billAmount;
 
+// Float properties (should move/repeat to model)
 @property (nonatomic) float billAmountValue;
 @property (nonatomic) float tipPercentageValue;
 
 
 @property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (weak, nonatomic) IBOutlet UITextField *tipPercentageTextField;
 @property (weak, nonatomic) IBOutlet UIButton *calculateTipButton;
 @property (weak, nonatomic) IBOutlet UILabel *tipAmountLabel;
-@property (weak, nonatomic) IBOutlet UITextField *tipPercentageTextField;
 
 - (IBAction)calculateTip:(id)sender;
 
@@ -39,25 +41,16 @@
 //    [[self tipPercentageTextField] setDelegate:self];
 //    self.tipPercentageTextField.delegate = self;
     
+    
     // Assign initial property values
     self.billAmountValue = 0.0;
     self.tipPercentageValue = 0.15;
     self.billAmountTextField.text = @"$0.00";
     self.tipAmountLabel.text = @"";
     self.tipPercentageTextField.text = @"15%";
-  
 }
 
-
-//-(void)textFieldDidEndEditing:(UITextField *)textField {
-//    NSLog(@"Text entered");
-//    if (textField == self.tipPercentageTextField) {
-////        [self changeTipPercentage];
-//    }
-//    ;
-//
-//}
-
+// Action Methods (should become calls to model instead)
 - (IBAction)calculateTip:(id)sender {
    //Calculate tip based on Bill amount and tip percentage
     self.billAmount = self.billAmountTextField.text;
@@ -73,6 +66,14 @@
     self.tipAmountLabel.text = [NSString stringWithFormat:@"You should tip: $%@",self.tipAmount];
 }
 
+//-(void)textFieldDidEndEditing:(UITextField *)textField {
+//    NSLog(@"Text entered");
+//    if (textField == self.tipPercentageTextField) {
+////        [self changeTipPercentage];
+//    }
+//    ;
+//
+//}
 
 
 
