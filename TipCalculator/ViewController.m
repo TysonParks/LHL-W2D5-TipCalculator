129b8af0c7e822d77ba09862e94c2b5b966223d2
tysonparks <typarks@gmail.com>
2018-02-23 12:17:10 -0800

    Created Calculate Tip button and corresponding input and action in ViewController

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (weak, nonatomic) IBOutlet UIButton *calculateTipButton;
 
 @end
 
@@ -17,12 +19,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)calculateTip:(id)sender {
+    
 }
 
 
